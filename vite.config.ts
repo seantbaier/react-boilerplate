c88cc4b07c4ad9ea8cca88e0664f8226225a57c3
Sean Baier <sean.t.baier@gmail.com>
2023-03-19 15:45:09 -0500

    setup: tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'url';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), eslint()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'test-utils': fileURLToPath(
+        new URL('./src/helpers/test-utils.tsx', import.meta.url)
+      ),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      provider: 'c8',
+    },
+    setupFiles: ['./vitest.setup.ts'],
+  },
+});
