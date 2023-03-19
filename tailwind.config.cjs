9ccf280e59d709d7c458e88b72b9d837ac7b4fc2
Sean Baier <sean.t.baier@gmail.com>
2023-03-19 15:20:56 -0500

    setup: tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const colors = require('tailwindcss/colors');
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+      extend: {
+        colors: {
+          ...colors,
+        },
+      },
+    },
+  plugins: [require('@tailwindcss/forms')],
+};
