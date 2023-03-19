0984ac610ca61f25cdb2484981fc3afb9b5d1aa6
Sean Baier <sean.t.baier@gmail.com>
2023-03-19 15:24:33 -0500

    setup: prettier

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,15 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const colors = require('tailwindcss/colors');
 
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-    theme: {
-      extend: {
-        colors: {
-          ...colors,
-        },
+  theme: {
+    extend: {
+      colors: {
+        ...colors,
       },
     },
+  },
   plugins: [require('@tailwindcss/forms')],
 };
