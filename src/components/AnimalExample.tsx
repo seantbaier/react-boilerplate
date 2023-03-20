cdc357092b7384debada60398c8a892d32980474
Sean Baier <sean.t.baier@gmail.com>
2023-03-20 03:24:58 -0500

    setup: adding api state

--- src/components/AnimalExample.tsx
+++ src/components/AnimalExample.tsx
@@ -1,24 +1,36 @@
-import { useFetchAnimals } from '@/hooks/useFetchAnimals';
+import { useEffect } from 'react';
 
-function AnimalExample() {
-  const { dog, cat, fetchAnimals } = useFetchAnimals();
+import { useFetchDog } from '@/hooks/useFetchDog';
+import LazySpinner from './LazySpinner';
+
+export default function AnimalExample() {
+  const {
+    dog,
+    initFetchDog,
+    isFetchDogStatusIdle,
+    isFetchDogStatusPending,
+    isFetchDogStatusError,
+    isFetchDogStatusSuccess,
+  } = useFetchDog();
+
+  useEffect(() => {
+    initFetchDog();
+  }, []);
 
   return (
     <div className='my-8 mx-auto max-w-2xl'>
-      <div className='flex gap-8'>
-        <div className='w-1/2'>
-          {cat ? (
-            <img className='h-64 w-full object-cover' src={cat} alt='Cat' />
-          ) : null}
-        </div>
-        <div className='w-1/2'>
-          {dog ? (
+      <div className='flex justify-center gap-8'>
+        <div className='w-64 h-64'>
+          {isFetchDogStatusIdle ? <p>Welcome</p> : null}
+          <LazySpinner show={isFetchDogStatusPending} delay={400} />
+          {isFetchDogStatusError ? <p>There was a problem</p> : null}
+          {isFetchDogStatusSuccess ? (
             <img className='h-64 w-full object-cover' src={dog} alt='Dog' />
           ) : null}
         </div>
       </div>
       <button
-        onClick={fetchAnimals}
+        onClick={initFetchDog}
         className='mt-4 bg-blue-800 text-blue-100 p-4'
       >
         Fetch animals
@@ -26,5 +38,3 @@ function AnimalExample() {
     </div>
   );
 }
-
-export default AnimalExample;
--- src/hooks/useFetchDog.tsx
+++ src/hooks/useFetchDog.tsx
@@ -1,16 +1,41 @@
-import { fetchDog } from '@/api/animalApi';
 import { useState } from 'react';
+
+import { fetchDog } from '@/api/animalApi';
+import { withAsync } from '@/utils/withAsync';
+import { IDLE, PENDING, SUCCESS, ERROR } from '@/api/constants/apiStatus';
+import { useApiStatus } from '@/api/hooks/useApiStatus';
 
 export const useFetchDog = () => {
   const [dog, setDog] = useState<string>();
 
+  const {
+    status: fetchDogStatus,
+    setStatus: setFetchDogStatus,
+    isIdle: isFetchDogStatusIdle,
+    isPending: isFetchDogStatusPending,
+    isError: isFetchDogStatusError,
+    isSuccess: isFetchDogStatusSuccess,
+  } = useApiStatus(IDLE);
+
   const initFetchDog = async () => {
-    const response = await fetchDog();
-    setDog(response.data.message);
+    setFetchDogStatus(PENDING);
+    const { response, error } = await withAsync(() => fetchDog());
+
+    if (error) {
+      setFetchDogStatus(ERROR);
+    } else if (response) {
+      setDog(response.data.message);
+      setFetchDogStatus(SUCCESS);
+    }
   };
 
   return {
     dog,
+    fetchDogStatus,
     initFetchDog,
+    isFetchDogStatusIdle,
+    isFetchDogStatusPending,
+    isFetchDogStatusError,
+    isFetchDogStatusSuccess,
   };
 };
